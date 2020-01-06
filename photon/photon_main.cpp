54462c1b2d7a3767615791a571879c3e0ab5284a
Milos Krsmanovic <miloskrsma@gmail.com>
2020-01-06 21:38:57 +0100

    Doradjen GUI.

--- photon/photon_main.cpp
+++ photon/photon_main.cpp
@@ -1,6 +1,7 @@
 #include "photon_main.h"
 #include "ui_photon_main.h"
 #include <rsimg/exception.hpp>
+#include <QFileDialog>
 
 photon_main::photon_main(QWidget *parent)
     : QMainWindow(parent)
@@ -17,8 +18,11 @@ photon_main::~photon_main()
 
 void photon_main::on_actionOtvori_triggered()
 {
+    QString nazivFajla = QFileDialog::getOpenFileName(this, "Otvorite fajl:");
+    //QFile fajl(nazivFajla);
+
     try {
-        this->m_imageUndo = PhotonUndo(rs::Image("lena.png"));
+        this->m_imageUndo = PhotonUndo(rs::Image(nazivFajla.toStdString()));
         ui->slika_glavno->setImage(m_imageUndo.qimagePtr());
         ui->slika_glavno->update();
     }  catch (const rs::Exception &e) {
@@ -38,4 +42,61 @@ void photon_main::on_actionPoni_ti_izmenu_undo_triggered()
 {
     m_imageUndo.undo();
     ui->slika_glavno->update();
+}
+
+void photon_main::on_actionObrtanje_flip_triggered()    //obrtanje po X osi
+{
+    m_imageUndo.action(rs::Flip(rs::Flip::Axis::X));
+    ui->slika_glavno->update();
+}
+
+void photon_main::on_actionObrtanje_y_osa_triggered()   //obrtanje po Y osi
+{
+    m_imageUndo.action(rs::Flip(rs::Flip::Axis::Y));
+    ui->slika_glavno->update();
+}
+
+void photon_main::on_actionSe_enje_triggered()
+{
+    m_imageUndo.action(rs::Crop(0,0,100,100));
+    ui->slika_glavno->update();
+}
+
+void photon_main::on_actionOsvetljenje_triggered()
+{
+//TODO
+}
+
+void photon_main::on_actionKontrast_triggered()
+{
+    m_imageUndo.action(rs::Contrast(50)); //TODO kako uhvatiti podatak sa slajdera?
+    ui->slika_glavno->update();
+}
+
+void photon_main::on_actionVrati_izmenu_redo_triggered()
+{
+    m_imageUndo.redo();
+    ui->slika_glavno->update();
+}
+
+void photon_main::on_actionIza_i_iz_programa_triggered()
+{
+    QApplication::exit();
+}
+
+void photon_main::on_primeni_clicked()  //primenjuje se osvetljenje i kontrast
+{
+    m_imageUndo.action(rs::Brightness(ui->osvetljenje_slider->value()));
+    m_imageUndo.action(rs::Contrast(ui->kontrast_slider->value()));
+    ui->slika_glavno->update();
+}
+
+void photon_main::on_obrni_x_clicked()
+{
+
+}
+
+void photon_main::on_actionSnimi_sa_uvaj_triggered()
+{
+    QString nazivFajla = QFileDialog::getSaveFileName(this, "Sačuvaj kao");
 }
--- photon/photon_main.h
+++ photon/photon_main.h
@@ -23,6 +23,26 @@ private slots:
 
     void on_actionPoni_ti_izmenu_undo_triggered();
 
+    void on_actionObrtanje_flip_triggered();
+
+    void on_actionObrtanje_y_osa_triggered();
+
+    void on_actionSe_enje_triggered();
+
+    void on_actionOsvetljenje_triggered();
+
+    void on_actionKontrast_triggered();
+
+    void on_actionVrati_izmenu_redo_triggered();
+
+    void on_actionIza_i_iz_programa_triggered();
+
+    void on_primeni_clicked();
+
+    void on_obrni_x_clicked();
+
+    void on_actionSnimi_sa_uvaj_triggered();
+
 private:
     Ui::photon_main *ui;
     PhotonUndo m_imageUndo;
